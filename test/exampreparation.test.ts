da0ff5321f5ab5bd5da066649c399fb9fdac6d55
martin8207 <martin.dragozov8207@gmail.com>
2024-11-19 20:27:07 +0200

    Initial commit

---
+++ test/exampreparation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Exampreparation from '../lib/exampreparation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/exampreparation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Exampreparation.ExampreparationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
